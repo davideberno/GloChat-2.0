49e1bdebffa381079e172c4815436fbfa4184675
Davide Bernocchi <davide.berno84@gmail.com>
2020-06-13 23:18:04 +0200

    deploy x Heroku

---
+++ client/build/precache-manifest.a2e7e3b255336373835710c7878297eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8f8509ada8e9af8586ba3e5f3421f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cede1e3df3c041f79b7d",
+    "url": "/static/css/main.e8668e46.chunk.css"
+  },
+  {
+    "revision": "5431015ba52a9e450351",
+    "url": "/static/js/2.5855ec5e.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.5855ec5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cede1e3df3c041f79b7d",
+    "url": "/static/js/main.31515158.chunk.js"
+  },
+  {
+    "revision": "ebe63e743c06a0c598ba",
+    "url": "/static/js/runtime-main.f5f6cf6b.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4e83d774730169fe3eaf284bf2a05fe.js"
+  "/precache-manifest.a2e7e3b255336373835710c7878297eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
